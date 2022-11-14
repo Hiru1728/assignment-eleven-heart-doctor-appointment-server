24e455aae1c276372408839447f311bfdcc33937
Hiru1728 <hirucse1728@gmail.com>
2022-11-14 21:23:00 +0600

    added specific id and load data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,7 +28,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
     }
     finally {
 
