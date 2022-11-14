de65e58fa41f3a34f1f1c05fcc46b0a00b326b1a
Hiru1728 <hirucse1728@gmail.com>
2022-11-14 20:24:58 +0600

    added limit and take 3 service

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yrpb3ds.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db('heartDoctor').collection('services');
+
+        app.get('/homeservices', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
+        })
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
+
+
 
 app.get('/', (req, res) => {
     res.send('Heart Doctor server is running');
