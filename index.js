ba40e67a946967209509bb99016dd0cd32517bf5
Hiru1728 <hirucse1728@gmail.com>
2022-11-16 16:52:41 +0600

    get the review for update

--- index.js
+++ index.js
@@ -13,7 +13,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yrpb3ds.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,
+        function (error, decoded) {
+            if (error) {
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+            req.decoded = decoded;
+            next();
+        })
+}
 
 async function run() {
     try {
@@ -69,6 +83,13 @@ async function run() {
             }
             const cursor = reviewsCollection.find(query);
             const review = await cursor.toArray();
+            res.send(review);
+        })
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const review = await reviewsCollection.findOne(query);
             res.send(review);
         })
 
